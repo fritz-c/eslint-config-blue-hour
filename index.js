2a82f2d67ceb010d3a332e7e35aa9f151e5fb671
fritz-c <fritz-c@users.noreply.github.com>
2016-08-05 11:11:00 +0900

    Add some more rules to ignore

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ module.exports = {
         'react/jsx-indent': [1, 4],
         'react/jsx-indent-props': [1, 4],
         'react/prefer-es6-class': 0,
+        'react/jsx-filename-extension': 0,
+        'react/prefer-stateless-function': 0,
+
+        'import/prefer-default-export': 0,
 
         'comma-dangle': 0,
         'one-var': 0,
